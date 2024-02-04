e0ee341fd00d146a28f821decea8cb9045b5f4c7
Amnish <amnishsingh04@gmail.com>
2024-02-04 12:01:10 -0500

    Design profile management page

--- src/app/pages/user-management-page/user-management-page.component.ts
+++ src/app/pages/user-management-page/user-management-page.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'user-management-page',
     templateUrl: './user-management-page.component.html',
     styleUrls: ['./user-management-page.component.scss'],
 })
-export class UserManagementPageComponent {}
+export class UserManagementPageComponent {
+    personalDetailsForm = this.fb.group({
+        firstName: [null, [Validators.required]],
+        lastName: [null, [Validators.required]],
+        phone: [
+            null,
+            [
+                Validators.required,
+                Validators.pattern(/^\d*$/),
+                Validators.maxLength(10),
+                Validators.minLength(10),
+            ],
+        ],
+        role: [null, [Validators.required]],
+    });
+
+    constructor(private fb: FormBuilder) {}
+}
