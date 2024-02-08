cc371b5d292e2aaefeba65e38203476ae980766e
Aparajeeta Diwya <adiwya@myseneca.ca>
2024-02-08 00:15:17 -0500

    Landing Page SCSS

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,14 +1,21 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'landing-page',
+    selector: 'app-landing-page',
     templateUrl: './landing-page.component.html',
     styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
     playTransition = false;
 
+    constructor(private router: Router) {}
+
     ngOnInit(): void {
         this.playTransition = true;
+    }
+
+    navigateToExploreRestaurants() {
+        this.router.navigate(['/explore-restaurants-page']);
     }
 }
