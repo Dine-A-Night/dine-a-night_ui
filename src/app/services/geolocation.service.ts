f96c55c3462bbe29b5096f63fd6e8bd6ab9c10ee
Amnish <amnishsingh04@gmail.com>
2024-02-18 22:27:58 -0500

    Show restaurant's current location in maps when editing

---
+++ src/app/services/geolocation.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { RestaurantLocation } from '../models/restaurant';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class GeolocationService {
+    private openStreetMapUrl =
+        'https://nominatim.openstreetmap.org/search?format=json';
+
+    constructor() {}
+
+    async getCoordinatesByAddress(address: RestaurantLocation): Promise<any> {
+        let url = new URL(this.openStreetMapUrl);
+
+        if (address.city.length) url.searchParams.append('city', address.city);
+        if (address.streetAddress.length)
+            url.searchParams.append('street', address.streetAddress);
+        if (address.province.length)
+            url.searchParams.append('state', address.province);
+        if (address.country.length)
+            url.searchParams.append('country', address.country);
+        if (address.postal.length)
+            url.searchParams.append('postal', address.postal);
+
+        const result = await (await fetch(url)).json();
+
+        console.log(result);
+
+        return result[0] ?? null;
+    }
+}
