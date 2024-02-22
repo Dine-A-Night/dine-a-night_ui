db75e2683c26ced460ad27640ad3f208c6d3a3c3
Amnish <amnishsingh04@gmail.com>
2024-02-21 22:53:00 -0500

    Set a longer timeout for geolocation

--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -47,7 +47,7 @@ export class GeolocationService {
                     },
                     {
                         enableHighAccuracy: false,
-                        timeout: 1000,
+                        timeout: 5000,
                     },
                 );
             } else {
