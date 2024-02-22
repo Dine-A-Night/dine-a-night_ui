9152bdb85f49e775944f96d45c444518a40641b4
Amnish <amnishsingh04@gmail.com>
2024-02-21 23:02:33 -0500

    Optimize geolocation performance

--- src/app/utils/static-helpers.ts
+++ src/app/utils/static-helpers.ts
@@ -1,3 +1,11 @@
 export function POSTAL_CODE_REGEX(): RegExp {
     return /^[A-Z]\d[A-Z] ?\d[A-Z]\d$/;
 }
+
+export function MINUTES_TO_MILLISECONDS(minutes: number) {
+    return SECONDS_TO_MILLISECONDS(minutes * 60);
+}
+
+export function SECONDS_TO_MILLISECONDS(seconds: number) {
+    return seconds * 1000;
+}
