7a785b30a5500fedbf472bac2978c4414c222749
Aparajeeta Diwya <adiwya@myseneca.ca>
2024-02-21 21:56:47 -0500

    Initial Restaurant Detail Layout

--- src/app/pages/restaurant-details-page/restaurant-details-page.component.ts
+++ src/app/pages/restaurant-details-page/restaurant-details-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription, filter } from 'rxjs';
 import { RestaurantAddEditComponent } from 'src/app/components/restaurants/restaurant-add-edit/restaurant-add-edit.component';
 import { Restaurant } from 'src/app/models/restaurant';
 import { UserRole } from 'src/app/models/user';
@@ -15,28 +15,38 @@ import { deepEqual } from 'src/app/utils/helper-functions';
     styleUrls: ['./restaurant-details-page.component.scss'],
 })
 export class RestaurantDetailsPageComponent implements OnInit, OnDestroy {
-    // Services
-    activatedRoute = inject(ActivatedRoute);
-    restaurantService = inject(RestaurantsService);
-    userService = inject(UserService);
-    dialog = inject(MatDialog);
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private restaurantService: RestaurantsService,
+        private userService: UserService,
+        private dialog: MatDialog,
+    ) {}
 
-    // Props
     paramSubscription: Subscription;
     restaurant: Restaurant;
+    description: string;
+    cuisines: string[];
+    location: string;
 
     ngOnInit(): void {
         this.paramSubscription = this.activatedRoute.params.subscribe(
             (params) => {
                 const restaurantId = params['id'];
 
-                console.log(restaurantId);
-
                 if (restaurantId) {
                     this.restaurantService
                         .getRestaurantById(restaurantId)
                         .subscribe((restaurant: Restaurant) => {
                             this.restaurant = restaurant;
+                            this.description = restaurant.description ?? ''; // Assign description from API response, handle nullish case
+                            this.cuisines =
+                                restaurant.cuisines?.map(
+                                    (cuisine) => cuisine.name,
+                                ) ?? [];
+                            // Assign cuisines from API response, handle nullish case
+                            this.location = this.formatLocation(
+                                restaurant.location,
+                            ); // Format location
                         });
                 }
             },
@@ -47,7 +57,12 @@ export class RestaurantDetailsPageComponent implements OnInit, OnDestroy {
         this.paramSubscription.unsubscribe();
     }
 
-    onEditClick() {
+    formatLocation(location: any): string {
+        // Implement your formatting logic here based on the structure of RestaurantLocation
+        return `${location.streetAddress}, ${location.city}, ${location.province}, ${location.country} ${location.postal}`;
+    }
+
+    onEditClick(): void {
         this.dialog
             .open(RestaurantAddEditComponent, {
                 data: {
@@ -58,22 +73,20 @@ export class RestaurantDetailsPageComponent implements OnInit, OnDestroy {
             })
             .afterClosed()
             .subscribe((result) => {
-                console.log(result);
-
                 if (!deepEqual(result, this.restaurant) && result) {
                     this.restaurant = result;
                 }
             });
     }
 
-    get coverImage() {
+    get coverImage(): string {
         return (
-            this.restaurant?.coverPhotoUri ??
+            this.restaurant?.coverPhotoUri ||
             RestaurantsService.DEFAULT_COVER_PHOTO_URI
         );
     }
 
-    get showEdit() {
+    get showEdit(): boolean {
         return (
             this.userService.getCurrentRole() === UserRole.ADMIN &&
             this.restaurant?.ownerId === this.userService.currentUser()?.uid
