e132f4dfc333f21b7f2e517f5ea823cec80919d7
Amnish <amnishsingh04@gmail.com>
2024-02-25 22:00:27 -0500

    Allow uploading cover photo for restaurants

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { firstValueFrom } from 'rxjs';
+import { Observable, catchError, firstValueFrom, of, switchMap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -17,5 +17,35 @@ export class FileUploadService {
         await uploadTask;
 
         return firstValueFrom(ref.getDownloadURL());
+    }
+
+    deleteFile(filePath: string): Observable<any> {
+        // Get a reference to the image in Firebase Storage
+        const imageRef = this.afStorage.ref(filePath);
+
+        // Get the download URL for the image
+        return imageRef.getDownloadURL().pipe(
+            catchError((error) => {
+                // If the error indicates the image was not found, handle it gracefully
+                if (error.code === 'storage/object-not-found') {
+                    console.log('Image not found at path:', filePath);
+                    // Return a observable indicating that the image was not found
+                    return of(null);
+                }
+                // For other errors, re-throw the error
+                throw error;
+            }),
+            // If no error occurred, proceed with the delete operation
+            switchMap((downloadURL) => {
+                if (downloadURL !== null) {
+                    // If the download URL exists, it means the image exists
+                    // Proceed with the delete operation
+                    return this.afStorage.refFromURL(downloadURL).delete();
+                } else {
+                    // Image not found, return an observable indicating no action needed
+                    return of(null);
+                }
+            }),
+        );
     }
 }
