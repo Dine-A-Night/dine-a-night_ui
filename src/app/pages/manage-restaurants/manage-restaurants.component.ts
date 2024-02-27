29dbd917c22146face068919b0c4cbb9ab34d46a
Amnish Singh Arora <amnishsingh04@gmail.com>
2024-02-27 01:16:46 -0500

    Make sure current user is available before requesting for owned restaurants

--- src/app/pages/manage-restaurants/manage-restaurants.component.ts
+++ src/app/pages/manage-restaurants/manage-restaurants.component.ts
@@ -1,51 +1,52 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
 import { Restaurants } from 'src/app/models/restaurant';
 import { RestaurantsService } from 'src/app/services/restaurants.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'manage-restaurants',
     templateUrl: './manage-restaurants.component.html',
     styleUrls: ['./manage-restaurants.component.scss'],
 })
-export class ManageRestaurantsComponent implements OnInit {
+export class ManageRestaurantsComponent {
     // Services
     restaurantsService = inject(RestaurantsService);
     notificationService = inject(MatSnackBar);
+    userService = inject(UserService);
 
     // Props
     restaurants: Restaurants;
     isLoading = false;
 
-    ngOnInit(): void {
-        // For now
-        this.getOwnedRestaurants();
-        // this.getAllRestaurants();
+    constructor() {
+        effect(() => {
+            if (this.userService.currentUser() && !this.restaurants) {
+                this.getOwnedRestaurants();
+            }
+        });
     }
 
     getOwnedRestaurants() {
         this.isLoading = true;
 
         // To make sure the current user data is already fetched
-        setTimeout(() => {
-            this.restaurantsService.getOwnedRestaurants().subscribe({
-                next: (restaurants) => {
-                    this.restaurants = restaurants ?? [];
-                    this.isLoading = false;
-                },
-                error: (err) => {
-                    this.notificationService.open(
-                        `Could not retrieve restaurants: ${err.message}`,
-                        'Ok',
-                        {
-                            duration: 4000,
-                        },
-                    );
-                    this.isLoading = false;
-                },
-            });
-        }, 500);
+        this.restaurantsService.getOwnedRestaurants().subscribe({
+            next: (restaurants) => {
+                this.restaurants = restaurants ?? [];
+                this.isLoading = false;
+            },
+            error: (err) => {
+                this.notificationService.open(
+                    `Could not retrieve restaurants: ${err.message}`,
+                    'Ok',
+                    {
+                        duration: 4000,
+                    },
+                );
+                this.isLoading = false;
+            },
+        });
     }
 
     // Just to test for now
