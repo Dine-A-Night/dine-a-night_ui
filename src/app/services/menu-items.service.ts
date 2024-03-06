e21fde6e0e68988aa97b18064a53089d295aacb4
Aparajeeta Diwya <adiwya@myseneca.ca>
2024-03-05 23:01:20 -0500

    Updated Menu Item Service

--- src/app/services/menu-items.service.ts
+++ src/app/services/menu-items.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MenuItem } from '../models/menu-item';
-import { environment } from 'src/environments/environment.development';
+import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
@@ -10,10 +11,29 @@ import { environment } from 'src/environments/environment.development';
 export class MenuItemsService {
     private API_URL = environment.apiUrl;
 
-    constructor(private http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private authService: AuthService,
+    ) {}
 
     getMenuItems(restaurantId: string): Observable<MenuItem[]> {
-        const url = `${this.API_URL}/restaurants/${restaurantId}/menuitems`;
-        return this.http.get<MenuItem[]>(url);
+        const url = `${this.API_URL}/restaurants/${restaurantId}/menu`;
+        const headers = this.authService.getAuthHeaders();
+        return this.http.get<MenuItem[]>(url, { headers });
+    }
+
+    addMenuItem(
+        restaurantId: string,
+        menuItem: MenuItem,
+    ): Observable<MenuItem> {
+        const url = `${this.API_URL}/restaurants/${restaurantId}/menu`;
+        const headers = this.authService.getAuthHeaders();
+        return this.http.post<MenuItem>(url, menuItem, { headers });
+    }
+
+    deleteMenuItem(menuItemId: string): Observable<any> {
+        const url = `${this.API_URL}/menuitems/${menuItemId}`;
+        const headers = this.authService.getAuthHeaders();
+        return this.http.delete(url, { headers });
     }
 }
