0208e34d615129f55b0f2325f5ea270e1a4b33f4
Amnish <amnishsingh04@gmail.com>
2024-03-07 02:17:34 -0500

    Extract restaurant details tab into its own component

---
+++ src/app/components/restaurants/restaurant-details-tab/restaurant-details-tab.component.ts
@@ -0,0 +1,99 @@
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    inject,
+} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Restaurant } from 'src/app/models/restaurant';
+import { RestaurantsService } from 'src/app/services/restaurants.service';
+import { deepEqual, isDefNotNull } from 'src/app/utils/helper-functions';
+import { RestaurantAddEditComponent } from '../restaurant-add-edit/restaurant-add-edit.component';
+
+@Component({
+    selector: 'restaurant-details-tab',
+    templateUrl: './restaurant-details-tab.component.html',
+    styleUrls: ['./restaurant-details-tab.component.scss'],
+})
+export class RestaurantDetailsTabComponent implements OnInit {
+    @Input() restaurant: Restaurant;
+    @Input() showEdit: boolean = false;
+
+    @Output() restaurantUpdated = new EventEmitter<Restaurant>();
+
+    restaurantService = inject(RestaurantsService);
+    notificationService = inject(MatSnackBar);
+    dialog = inject(MatDialog);
+
+    ngOnInit(): void {}
+
+    onEditClick(): void {
+        this.dialog
+            .open(RestaurantAddEditComponent, {
+                data: {
+                    isEdit: true,
+                    restaurant: this.restaurant,
+                },
+                panelClass: ['overflow-y-auto', 'h-4/5'],
+            })
+            .afterClosed()
+            .subscribe((result) => {
+                if (!deepEqual(result, this.restaurant) && result) {
+                    this.restaurant = result;
+                    this.restaurantUpdated.emit(this.restaurant);
+                }
+            });
+    }
+
+    uploadCoverPhoto(event) {
+        const files = event; // Get the selected files
+
+        const imageFile = [...files].filter((file) =>
+            file.type.startsWith('image/'),
+        )[0];
+
+        if (isDefNotNull(imageFile)) {
+            this.restaurantService
+                .uploadCoverPhoto(this.restaurant._id, imageFile)
+                .subscribe({
+                    next: (imageUrl) => {
+                        this.restaurant = {
+                            ...this.restaurant,
+                            coverPhotoUri: imageUrl,
+                        };
+                        this.restaurantUpdated.emit(this.restaurant);
+
+                        this.notificationService.open(
+                            'Successfully uploaded cover photo',
+                            'Ok',
+                            {
+                                duration: 3000,
+                            },
+                        );
+                    },
+                    error: (err: any) => {
+                        this.notificationService.open(
+                            `Failed to upload cover photo: ${err.message}`,
+                            'Oops',
+                            {
+                                duration: 3000,
+                            },
+                        );
+                    },
+                });
+        } else {
+            this.notificationService.open(
+                'Make sure you select an image file for upload!',
+                'Ok',
+            );
+        }
+    }
+
+    formatLocation(location: any): string {
+        // Implement your formatting logic here based on the structure of RestaurantLocation
+        return `${location.streetAddress}, ${location.city}, ${location.province}, ${location.country} ${location.postal}`;
+    }
+}
--- src/app/pages/restaurant-details-page/restaurant-details-page.component.ts
+++ src/app/pages/restaurant-details-page/restaurant-details-page.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription, filter } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
-import { RestaurantAddEditComponent } from 'src/app/components/restaurants/restaurant-add-edit/restaurant-add-edit.component';
+import { Subscription } from 'rxjs';
 import { Restaurant } from 'src/app/models/restaurant';
 import { UserRole } from 'src/app/models/user';
 import { RestaurantsService } from 'src/app/services/restaurants.service';
 import { UserService } from 'src/app/services/user.service';
-import { deepEqual, isDefNotNull } from 'src/app/utils/helper-functions';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MenuItemsComponent } from 'src/app/components/menu-items/menu-items.component';
 
 @Component({
     selector: 'restaurant-details-page',
@@ -21,15 +16,11 @@ export class RestaurantDetailsPageComponent implements OnInit, OnDestroy {
         private activatedRoute: ActivatedRoute,
         private restaurantService: RestaurantsService,
         private userService: UserService,
-        private dialog: MatDialog,
-        private notificationService: MatSnackBar,
     ) {}
 
     paramSubscription: Subscription;
     restaurant: Restaurant;
-    description: string;
     cuisines: string[];
-    location: string;
 
     ngOnInit(): void {
         this.paramSubscription = this.activatedRoute.params.subscribe(
@@ -41,15 +32,6 @@ export class RestaurantDetailsPageComponent implements OnInit, OnDestroy {
                         .getRestaurantById(restaurantId)
                         .subscribe((restaurant: Restaurant) => {
                             this.restaurant = restaurant;
-                            this.description = restaurant.description ?? ''; // Assign description from API response, handle nullish case
-                            this.cuisines =
-                                restaurant.cuisines?.map(
-                                    (cuisine) => cuisine.name,
-                                ) ?? [];
-                            // Assign cuisines from API response, handle nullish case
-                            this.location = this.formatLocation(
-                                restaurant.location,
-                            ); // Format location
                         });
                 }
             },
@@ -60,73 +42,8 @@ export class RestaurantDetailsPageComponent implements OnInit, OnDestroy {
         this.paramSubscription.unsubscribe();
     }
 
-    formatLocation(location: any): string {
-        // Implement your formatting logic here based on the structure of RestaurantLocation
-        return `${location.streetAddress}, ${location.city}, ${location.province}, ${location.country} ${location.postal}`;
-    }
-
-    onEditClick(): void {
-        this.dialog
-            .open(RestaurantAddEditComponent, {
-                data: {
-                    isEdit: true,
-                    restaurant: this.restaurant,
-                },
-                panelClass: ['overflow-y-auto', 'h-4/5'],
-            })
-            .afterClosed()
-            .subscribe((result) => {
-                if (!deepEqual(result, this.restaurant) && result) {
-                    this.restaurant = result;
-                }
-            });
-    }
-
-    uploadCoverPhoto(event) {
-        const files = event; // Get the selected files
-
-        const imageFile = [...files].filter((file) =>
-            file.type.startsWith('image/'),
-        )[0];
-
-        if (isDefNotNull(imageFile)) {
-            this.restaurantService
-                .uploadCoverPhoto(this.restaurant._id, imageFile)
-                .subscribe({
-                    next: (imageUrl) => {
-                        this.restaurant = {
-                            ...this.restaurant,
-                            coverPhotoUri: imageUrl,
-                        };
-
-                        this.notificationService.open(
-                            'Successfully uploaded cover photo',
-                            'Ok',
-                            {
-                                duration: 3000,
-                            },
-                        );
-                    },
-                    error: (err: any) => {
-                        this.notificationService.open(
-                            `Failed to upload cover photo: ${err.message}`,
-                            'Oops',
-                            {
-                                duration: 3000,
-                            },
-                        );
-                    },
-                });
-        } else {
-            this.notificationService.open(
-                'Make sure you select an image file for upload!',
-                'Ok',
-            );
-        }
-    }
-
     restaurantUpdated(restaurant: Restaurant) {
-        this.restaurant = restaurant;
+        this.restaurant = { ...restaurant };
     }
 
     get coverImage(): string {
