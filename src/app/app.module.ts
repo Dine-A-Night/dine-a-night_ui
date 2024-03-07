73b0db367caf517e2c061b8bc3253c923ba9a500
Amnish <amnishsingh04@gmail.com>
2024-03-07 01:51:08 -0500

    Refactor and fix menu items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,54 +6,55 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { environment } from 'src/environments/environment.development';
 import { LoginComponent } from './components/auth/login/login.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatRadioModule } from '@angular/material/radio';
-import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { FooterComponent } from './components/footer/footer.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { UserManagementPageComponent } from './pages/user-management-page/user-management-page.component';
-import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
-import { ExploreRestaurantsPageComponent } from './pages/explore-restaurants-page/explore-restaurants-page.component';
-import { ManageRestaurantsComponent } from './pages/manage-restaurants/manage-restaurants.component';
 import { ReAuthenticateDialogComponent } from './components/auth/re-authenticate-dialog/re-authenticate-dialog.component';
-import { RestaurantCardComponent } from './components/restaurants/restaurant-card/restaurant-card.component';
-import { RestaurantAddEditComponent } from './components/restaurants/restaurant-add-edit/restaurant-add-edit.component';
-import { MatSelectModule } from '@angular/material/select';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { AppAddressFormComponent } from './components/maps/app-address-form/app-address-form.component';
-import { ConfirmDialogComponent } from './components/reusables/confirm-dialog/confirm-dialog.component';
-import { RestaurantDetailedCardComponent } from './components/restaurants/restaurant-detailed-card/restaurant-detailed-card.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 import { CuisineCardComponent } from './components/cuisines/cuisine-card/cuisine-card.component';
-import { RestaurantDetailsPageComponent } from './pages/restaurant-details-page/restaurant-details-page.component';
-import { LoadingSpinnerComponent } from './components/reusables/loading-spinner/loading-spinner.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { FileSelectorComponent } from './components/reusables/file-selector/file-selector.component';
-import { RestaurantImagesComponent } from './components/restaurants/restaurant-images/restaurant-images.component';
-import { ImagesGridComponent } from './components/reusables/images/images-grid/images-grid.component';
-import { ImagePreviewModalComponent } from './components/reusables/images/image-preview-modal/image-preview-modal.component';
-import { ReviewsComponent } from './components/restaurants/reviews/reviews.component';
-import { ReviewComponent } from './components/restaurants/reviews/review/review.component';
-import { RatingSelectorComponent } from './components/restaurants/reviews/rating-selector/rating-selector.component';
-import { AddReviewFormComponent } from './components/restaurants/reviews/add-review-form/add-review-form.component';
-import { MenuItemsComponent } from './components/menu-items/menu-items.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppAddressFormComponent } from './components/maps/app-address-form/app-address-form.component';
 import { AddMenuItemDialogComponent } from './components/menu-items/menu-item/add-menu-item-dialog/add-menu-item-dialog.component';
+import { MenuItemComponent } from './components/menu-items/menu-item/menu-item.component';
+import { MenuItemsComponent } from './components/menu-items/menu-items.component';
+import { RestaurantAddEditComponent } from './components/restaurants/restaurant-add-edit/restaurant-add-edit.component';
+import { RestaurantCardComponent } from './components/restaurants/restaurant-card/restaurant-card.component';
+import { RestaurantDetailedCardComponent } from './components/restaurants/restaurant-detailed-card/restaurant-detailed-card.component';
+import { RestaurantImagesComponent } from './components/restaurants/restaurant-images/restaurant-images.component';
+import { AddReviewFormComponent } from './components/restaurants/reviews/add-review-form/add-review-form.component';
+import { RatingSelectorComponent } from './components/restaurants/reviews/rating-selector/rating-selector.component';
+import { ReviewComponent } from './components/restaurants/reviews/review/review.component';
+import { ReviewsComponent } from './components/restaurants/reviews/reviews.component';
+import { ConfirmDialogComponent } from './components/reusables/confirm-dialog/confirm-dialog.component';
+import { FileSelectorComponent } from './components/reusables/file-selector/file-selector.component';
+import { ImagePreviewModalComponent } from './components/reusables/images/image-preview-modal/image-preview-modal.component';
+import { ImagesGridComponent } from './components/reusables/images/images-grid/images-grid.component';
+import { LoadingSpinnerComponent } from './components/reusables/loading-spinner/loading-spinner.component';
+import { ExploreRestaurantsPageComponent } from './pages/explore-restaurants-page/explore-restaurants-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ManageRestaurantsComponent } from './pages/manage-restaurants/manage-restaurants.component';
+import { RestaurantDetailsPageComponent } from './pages/restaurant-details-page/restaurant-details-page.component';
+import { UserManagementPageComponent } from './pages/user-management-page/user-management-page.component';
 
 @NgModule({
     declarations: [
@@ -85,6 +86,7 @@ import { AddMenuItemDialogComponent } from './components/menu-items/menu-item/ad
         AddReviewFormComponent,
         MenuItemsComponent,
         AddMenuItemDialogComponent,
+        MenuItemComponent,
     ],
     imports: [
         BrowserModule,
--- src/app/models/menu-item.ts
+++ src/app/models/menu-item.ts
@@ -1,8 +1,17 @@
-export interface MenuItem {
-    _id?: string; // Optional if you include the _id in your responses
-    restaurantId: string; // Assuming restaurantId is a string in your frontend
-    name: string;
-    description: string;
-    unitPrice: number;
+export class MenuItem {
+    _id?: string;
+    restaurantId?: string;
+    name?: string;
+    description?: string;
+    unitPrice?: number;
     imageUri?: string;
+
+    constructor(options: Partial<MenuItem> = {}) {
+        this._id = options._id;
+        this.restaurantId = options.restaurantId;
+        this.name = options.name;
+        this.description = options.description;
+        this.unitPrice = options.unitPrice;
+        this.imageUri = options.imageUri;
+    }
 }
