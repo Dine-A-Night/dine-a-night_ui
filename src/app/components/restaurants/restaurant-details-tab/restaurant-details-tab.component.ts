075c31360105206e662be06793bb6593cf9951ef
Amnish <amnishsingh04@gmail.com>
2024-03-27 22:08:02 -0400

    Show google maps preview in restaurant detailed view

--- src/app/components/restaurants/restaurant-details-tab/restaurant-details-tab.component.ts
+++ src/app/components/restaurants/restaurant-details-tab/restaurant-details-tab.component.ts
@@ -4,19 +4,28 @@ import {
     Input,
     OnInit,
     Output,
+    ViewChild,
+    effect,
     inject,
 } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Restaurant } from 'src/app/models/restaurant.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+    Restaurant,
+    RestaurantLocation,
+} from 'src/app/models/restaurant.model';
+import { UserRole } from 'src/app/models/user.model';
+import { GeolocationService } from 'src/app/services/geolocation.service';
+import { GoogleMapsService } from 'src/app/services/google-maps.service';
 import { RestaurantsService } from 'src/app/services/restaurants.service';
+import { UserService } from 'src/app/services/user.service';
 import { deepEqual, isDefNotNull } from 'src/app/utils/helper-functions';
+import { CreateReservationDialogComponent } from '../../reservations/create-reservation-dialog/create-reservation-dialog.component';
 import { RestaurantAddEditComponent } from '../restaurant-add-edit/restaurant-add-edit.component';
 import { RestaurantLayoutBuilderComponent } from '../restaurant-layout-builder/restaurant-layout-builder.component';
-import { UserService } from 'src/app/services/user.service';
-import { UserRole } from 'src/app/models/user.model';
-import { CreateReservationDialogComponent } from '../../reservations/create-reservation-dialog/create-reservation-dialog.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { SpinnerType } from '../../reusables/loading-spinner/loading-spinner.component';
 
 @Component({
     selector: 'restaurant-details-tab',
@@ -65,6 +74,15 @@ export class RestaurantDetailsTabComponent implements OnInit {
                 if (!deepEqual(result, this.restaurant) && result) {
                     this.restaurant = result;
                     this.restaurantUpdated.emit(this.restaurant);
+
+                    const location = this.restaurant.location;
+
+                    if (location.coordinates.lat && location.coordinates.lng) {
+                        this.setMarkerPosition(
+                            location.coordinates.lat,
+                            location.coordinates.lng,
+                        );
+                    }
                 }
             });
     }
@@ -175,4 +193,95 @@ export class RestaurantDetailsTabComponent implements OnInit {
             relativeTo: this.activatedRoute,
         });
     }
+
+    //#region Google Maps
+
+    spinnerType = SpinnerType.PAN;
+
+    @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+
+    mapZoom = 12;
+    mapCenter: google.maps.LatLngLiteral;
+    mapOptions: google.maps.MapOptions = {};
+
+    mapMarker: any;
+
+    apiLoaded = this.googleMapsService.apiLoaded;
+
+    constructor(
+        private googleMapsService: GoogleMapsService,
+        private geolocationService: GeolocationService,
+    ) {
+        effect(() => {
+            if (this.apiLoaded()) {
+                this.mapMarker = {
+                    label: <google.maps.MarkerLabel>{
+                        text: 'Restaurant Location',
+                        className: 'font-bold relative top-8 text-red-600',
+                    },
+                    title: 'Restaurant Location',
+                    options: <google.maps.MarkerOptions>{
+                        animation: google.maps.Animation.DROP,
+                        draggable: false,
+                        clickable: true,
+                    },
+                };
+
+                // Make sure addressForm is loaded
+                setTimeout(async () => {
+                    // Only do this after api is loaded
+                    if (this.restaurant.location.streetAddress?.length) {
+                        // Use the restaurant's current location in case of edit
+                        const location =
+                            await this.geolocationService.getCoordinatesByAddress(
+                                this.restaurant.location as RestaurantLocation,
+                            );
+
+                        this.mapCenter = {
+                            lat: parseFloat(location.lat),
+                            lng: parseFloat(location.lon),
+                        };
+
+                        this.setMarkerPosition(
+                            parseFloat(location.lat),
+                            parseFloat(location.lon),
+                        );
+                    } else {
+                        // Center at user's current location (This should be in create)
+                        navigator.geolocation.getCurrentPosition(
+                            (position) => {
+                                this.mapCenter = {
+                                    lat: position.coords.latitude,
+                                    lng: position.coords.longitude,
+                                };
+
+                                this.setMarkerPosition(
+                                    position.coords.latitude,
+                                    position.coords.longitude,
+                                );
+
+                                document
+                                    .getElementById('google-maps')
+                                    ?.scrollIntoView();
+                            },
+                            undefined,
+                            {
+                                enableHighAccuracy: false,
+                                timeout: 5000,
+                                maximumAge: Infinity,
+                            },
+                        );
+                    }
+                }, 0);
+            }
+        });
+    }
+
+    setMarkerPosition(lat: number, lng: number) {
+        this.mapMarker.position = {
+            lat,
+            lng,
+        };
+    }
+    //#endregion
 }
